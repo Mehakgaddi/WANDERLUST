00e046f150b43751ec6c1c5880ca1251f1614cb7
Mehak <mehakgaddi867@gmail.com>
2024-11-29 21:56:33 +0530

    update

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const Listing = require("./models/listing.js");
 const port = 8080;
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
@@ -19,6 +20,20 @@ async function main() {
 
 app.get("/", (req, res) => {
   res.send("this is root path");
+});
+
+app.get("/testlisting", async (req, res) => {
+  let sampleListing = new Listing({
+    title: "My New Villa",
+    description: "By the beach",
+    price: 1200,
+    location: "Calangute Goa",
+    country: "India",
+  });
+
+  await sampleListing.save();
+  console.log("sample has been saved");
+  res.send("sample test listing");
 });
 
 app.listen(port, () => {
---
+++ models/listing.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  image: {
+    type: String,
+    // jb image ka url to hai but image nhi hai..........
+    default:
+      "https://unsplash.com/photos/3d-render-of-luxury-hotel-lobby-and-reception-FNAURWZ6Mqc",
+    // jb image undefined hai yah fir hai hi nhi........
+    set: (v) =>
+      v === ""
+        ? "https://unsplash.com/photos/3d-render-of-luxury-hotel-lobby-and-reception-FNAURWZ6Mqc"
+        : v,
+  },
+  price: {
+    type: Number,
+  },
+  location: {
+    type: String,
+  },
+  country: {
+    type: String,
+  },
+});
+
+const Listing = mongoose.model("listing", listingSchema);
+module.exports = Listing;
